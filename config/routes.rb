caa26eb9c1602dfdf727161884d1393c0c02e7d9
e1434mye1434my <59134132+e1434mye1434my@users.noreply.github.com>
2020-01-16 15:32:38 +0900

    aws updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
